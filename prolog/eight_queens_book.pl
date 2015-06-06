93c42fd10af2f994a16f458633cde5375a577fef
Leon Augustine <laugusti@mail.usf.edu>
2015-06-06 23:18:04 +0000

    Chapter 4 Prolog Day 3 - Optimzed Book Solution for eight queens problem

--- prolog/eight_queens_book.pl
+++ prolog/eight_queens_book.pl
@@ -1,17 +1,12 @@
 :-use_module(library(clpfd)).
-valid_queen((Row, Col)) :-
-	Range = [1,2,3,4,5,6,7,8],
-	member(Row, Range),
-	member(Col, Range).
+valid_queen((_, Col)) :-
+	member(Col, [1,2,3,4,5,6,7,8]).
 
 valid_board([]).
 valid_board([Head|Tail]) :-
 	valid_queen(Head),
 	valid_board(Tail).
 
-rows([], []).
-rows([(Row, _)|QueensTail], [Row|RowsTail]) :-
-	rows(QueensTail, RowsTail).
 
 cols([], []).
 cols([(_, Col)|QueensTail], [Col|ColsTail]) :-
@@ -28,15 +23,13 @@ diags2([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
 	diags2(QueensTail, DiagonalsTail).
 
 eight_queens(Board) :-
-	length(Board, 8),
+	Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
 	valid_board(Board),
 
-	rows(Board, Rows),
 	cols(Board, Cols),
 	diags1(Board, Diags1),
 	diags2(Board, Diags2),
 
-	all_different(Rows),
 	all_different(Cols),
 	all_different(Diags1),
 	all_different(Diags2).
